7caf2d7aeed3653e9ed6043950d450e17bb986ad
Arbind Gupta <131443775+arbind17128@users.noreply.github.com>
2023-06-13 11:53:32 +0545

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ function converter(e) {
     inputValue=e.target.value;
     document.getElementById('goutput').innerHTML=(inputValue/0.0022046).toFixed(2);
     document.getElementById('kgoutput').innerHTML=(inputValue/2.2046).toFixed(2);
-    document.getElementById('ozgrames').innerHTML=inputValue*16;
+    document.getElementById('ozgrames').innerHTML=(inputValue*16).toFixed(2);
     //toFixed() is used for two decimal value only after point
 }
 
