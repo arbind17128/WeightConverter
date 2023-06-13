6d67939c7c53bf06699bc2cdf4c8a152305ca367
Arbind Gupta <131443775+arbind17128@users.noreply.github.com>
2023-06-13 18:58:23 +0545

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,11 +4,19 @@ input.addEventListener('input', converter);
 document.getElementById('output').style.visibility='hidden';
 
 function converter(e) {
-    document.getElementById('output').style.visibility='visible';
-    inputValue=e.target.value;
-    document.getElementById('goutput').innerHTML=(inputValue/0.0022046).toFixed(2);
-    document.getElementById('kgoutput').innerHTML=(inputValue/2.2046).toFixed(2);
-    document.getElementById('ozgrames').innerHTML=(inputValue*16).toFixed(2);
-    //toFixed() is used for two decimal value only after point
+    if(e.target.value=== ''){
+        document.getElementById('output').style.visibility='hidden';
+
+    }
+    else{
+        document.getElementById('output').style.visibility='visible';
+        let inputValue=e.target.value;
+        document.getElementById('goutput').innerHTML=(inputValue/0.0022046).toFixed(2);
+        document.getElementById('kgoutput').innerHTML=(inputValue/2.2046).toFixed(2);
+        document.getElementById('ozgrames').innerHTML=(inputValue*16).toFixed(2);
+        //toFixed() is used for two decimal value only after point
+        
+    }
+ 
 }
 
